5c2543b6c79d9138277bea4e8fe133addef28a79
Evodiatembi <evodytembi@gmail.com>
2024-05-23 21:01:00 +0100

    getactivity.php

---
+++ Backend/getsteps.php
@@ -0,0 +1,11 @@
+<?php
+include "connect.php";
+
+
+$user = mysqli_query($db,"SELECT * FROM `steps` WHERE 1");
+$data = array();
+while($row = mysqli_fetch_object($user)){
+    $data[] = $row;
+}
+echo json_encode($data);
+?>
