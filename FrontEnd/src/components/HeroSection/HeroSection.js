7167ac77cdda9a484d738a58c10f295d50b6f660
Daniel <drewaju.16@gmail.com>
2024-06-06 01:22:44 +0100

    Hero

--- FrontEnd/src/components/HeroSection/HeroSection.js
+++ FrontEnd/src/components/HeroSection/HeroSection.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './HeroSection.css';
+// import { Heading } from '@chakra-ui/react';
 
 function HeroSection() {
   return (
     <section id="hero" className="hero-section">
       <div className="container">
-        <h2>Welcome to Our Landing Page</h2>
+        <h1>Welcome to FitGuage</h1>
         <p>Your success starts here. Discover our features and benefits.</p>
         <Link to="/signup">
           <button>Sign Up</button>
